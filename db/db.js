d100fea0eb938aed84e184db4addfca131b5c9a6
Mariana Moraes <mraes.mariana@gmail.com>
2022-08-20 09:36:34 -0300

    Criando banco de dados na nuvem

--- db/db.js
+++ db/db.js
@@ -1,9 +1,10 @@
 const {Sequelize} = require('sequelize')
 
-const sequelize = new Sequelize('mylists', 'root', '', {
-    host: 'localhost',
+const sequelize = new Sequelize('heroku_3dd571ae227a4c2', 'b0eeb880bdaa08', '64ff4432', {
+    host: 'us-cdbr-east-06.cleardb.net',
     dialect: 'mysql'
 })
 
 module.exports = sequelize
+
 
